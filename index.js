52935567a02e64a7b9f6611296f472efab626ba1
johney <johneyburke@gmail.com>
2023-09-10 22:32:46 -0600

    tough lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+
+    const firstTwoDrivers = drivers.slice(0, 2);
+
+    return firstTwoDrivers;
+  };
+
+  const returnLastTwoDrivers = (drivers) => {
+
+    const lastTwoDrivers = drivers.slice(2);
+
+    return lastTwoDrivers;
+  };
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+ 
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(arrayOfDrivers, afunction){
+    if (afunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+      } else {
+        return returnLastTwoDrivers(arrayOfDrivers);
+      }
+
+  }
